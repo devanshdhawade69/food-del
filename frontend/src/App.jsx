ea4528843fa1b0ffe828246f31313c49bdf208f4
Devansh Dhawade <rizzydess16@gmail.com>
2026-03-02 00:31:40 +0530

    sunday updates stripe integration and order page issue remaining

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,23 @@ import Cart from "./pages/cart/Cart";
 import PlaceOrder from "./pages/placeOrder/PlaceOrder";
 import Footer from "./components/footer/footer";
 import LoginPopup from "./components/loginPopup/LoginPopup";
+import Verify from "./pages/verify/Verify";
+import MyOrders from "./pages/MyOrders/MyOrders";
 
 const App = () => {
-
-  const [showLogin,setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/myorders" element={<MyOrders />} />
         </Routes>
       </div>
       <Footer />
